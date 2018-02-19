a30004697fcab68dabbc4c57047dd48de2573dd9
almogrubi <almog.rubinstein@cybereason.com>
2018-02-19 18:08:57 +0200

    loops

--- src/main/WhileFor.java
+++ src/main/WhileFor.java
@@ -11,10 +11,13 @@ public class WhileFor {
 
         //print each digit of the number 1237653
         int t = 123654;
-        while (t>0){
-            System.out.println(t%10);
-            t=t/10;
+        while (t > 0) {
+            System.out.println(t % 10);
+            t = t / 10;
         }
+
+        System.out.println("");
+        printNum(123654);
 
         int i, j, k;
         for (i = 0; i <= 6; i++) {
@@ -36,11 +39,20 @@ public class WhileFor {
         for (i = 0; i <= 6; i++) {
             for (j = 1; j <= 7 - i; j++)
                 System.out.print(" ");
-            for (k = 7-i; k <= 7; k++)
+            for (k = 7 - i; k <= 7; k++)
                 System.out.print("*");
             System.out.println("");
         }
 
+
+    }
+
+    private static void printNum(int i) {
+
+        if (i > 0) {
+            System.out.println(i % 10);
+            printNum(i/10);
+        }
 
     }
 }
