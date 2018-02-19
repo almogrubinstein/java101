f66b670d7fe8dc660d79d125e192e838f808addf
almogrubi <almog.rubinstein@cybereason.com>
2018-02-19 17:53:41 +0200

    loops

--- src/main/WhileFor.java
+++ src/main/WhileFor.java
@@ -9,8 +9,29 @@ public class WhileFor {
 //        while (false)
 //            System.out.println("Hello, World!");
 
-        
+        int i, j, k;
+        for (i = 0; i <= 6; i++) {
+            for (j = 1; j <= 7; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
 
+        System.out.println("");
+
+        for (i = 0; i <= 6; i++) {
+            for (j = 1; j <= 7 - i; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+
+        System.out.println("");
+
+        for (i = 0; i <= 6; i++) {
+            for (j = 1; j <= 7 - i; j++)
+                System.out.print(" ");
+            for (k = 7-i; k <= 7; k++)
+                System.out.print("*");
+            System.out.println("");
+        }
     }
-
 }
